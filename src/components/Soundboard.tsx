ae465f22e94ad7cc41806eec15bd64b3ed6aa4ab
wickathou <javier@enddesign.co>
2025-02-21 20:46:31 +0200

    Added rizz

--- src/components/Soundboard.tsx
+++ src/components/Soundboard.tsx
@@ -18,6 +18,7 @@ const DEFAULT_SOUNDS = [
   { id: '1', name: 'bruh', src: '/sounds/bruh.mp3', category: 'Meme' },
   { id: '2', name: 'boom', src: '/sounds/boom.mp3', category: 'Meme' },
   { id: '3', name: 'that sus', src: '/sounds/dramatic_hell_kitchens.mp3', category: 'Meme' },
+  { id: '4', name: 'Rizz', src: '/sounds/rizz.mp3', category: 'Meme' },
   { id: '4', name: 'OMG Hell No', src: '/sounds/omg_hell_no.mp3', category: 'Meme' },
 ];
 
