3e5cf44f56cbb7711abbd5a60533816b9f8b396b
wickathou <javier@enddesign.co>
2025-02-22 13:33:50 +0200

    Added useRef

--- src/components/soundboard/AddSoundForm.tsx
+++ src/components/soundboard/AddSoundForm.tsx
@@ -1,5 +1,4 @@
-
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { Button } from '../ui/button';
 import { Input } from '../ui/input';
 import { Mic, Square } from 'lucide-react';
