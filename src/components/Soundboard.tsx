7028a59b2d5219252ca8a6d334f312eaa1233dd9
wickathou <javier@enddesign.co>
2025-02-22 13:25:43 +0200

    Added damn son meme voice

--- src/components/Soundboard.tsx
+++ src/components/Soundboard.tsx
@@ -20,6 +20,7 @@ const DEFAULT_SOUNDS = [
   { id: '3', name: 'that sus', src: '/sounds/dramatic_hell_kitchens.mp3', category: 'Meme' },
   { id: '4', name: 'Rizz', src: '/sounds/rizz.mp3', category: 'Meme' },
   { id: '5', name: 'OMG Hell No', src: '/sounds/omg_hell_no.mp3', category: 'Meme' },
+  { id: '5', name: 'Damn son!', src: '/sounds/damn_son.mp3', category: 'Meme' },
 ];
 
 const Soundboard = () => {
